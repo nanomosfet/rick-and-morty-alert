ea2e7cc1036eee07613576fa0feb44b8cb6f05b2
root <root@ip-172-31-19-243.us-west-2.compute.internal>
2017-08-14 01:00:25 +0000

    added logging

--- rss-update-alert.py
+++ rss-update-alert.py
@@ -1,20 +1,32 @@
 from urllib2 import urlopen
 import xml.etree.ElementTree as ET
 import requests
+import time
+import os
+import datetime
 
-rss_xml = urlopen(
-    'https://iptorrents.com/torrents/rss?u=379571;tp=10a38607b96146711f6191a48263c5e3;78;23;25;66;82;65;79;22;5;99')\
-    .read()
+def read_feed():
+    write_log("reading feed")
+    return urlopen(
+        'https://iptorrents.com/torrents/rss?u=379571;tp=10a38607b96146711f6191a48263c5e3;78;23;25;66;82;65;79;22;5;99')\
+        .read()
+def write_log(s):    
+    with open('logfile.out', 'a+') as f:
+        f.write('time: %s Action: %s \n' % (str(datetime.datetime.now()), s))
+
 
 # tree = ET.parse(rss_xml)
 # root = tree.getroot()
 
-root = ET.fromstring(rss_xml)
-
+root = ET.fromstring(read_feed())
+write_log("Starting search")
 while True:
+    time.sleep(30)
+    root = ET.fromstring(read_feed())
     for item in root.findall('channel/item'):
-       title = item.find('title').text
-       if 'Rick and Morty' and 'S03E04' in title:
+        title = item.find('title').text
+        if 'Rick and Morty' and 'S03E04' in title:
+            write_log("Found Morty")
             r = requests.post(
                 'http://timothy-z-searcy.com/email_form',
                 data={
@@ -22,3 +34,4 @@ while True:
                     'email' : 'Test 2 tim',
                     'request' : 'rick'
                 })
+            break
